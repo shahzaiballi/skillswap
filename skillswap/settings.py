541f8037df905f1caedf4eb110c8bee6267a3296
Shahzaib Ali <shahzaib8157@gmail.com>
2025-01-19 13:30:58 +0500

    Update database

--- skillswap/settings.py
+++ skillswap/settings.py
@@ -55,19 +55,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'skillswap.wsgi.application'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'skillswap_db',
-        'USER': 'root',
-        'PASSWORD': 'Shaibiali@1290',
-        'HOST': 'localhost',  # or your MySQL server host
-        'PORT': '3306',      # default MySQL port
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
+DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / "db.sqlite3", } }
 
 
 AUTH_PASSWORD_VALIDATORS = [
